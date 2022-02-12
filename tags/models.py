43a837437f59392f604df7d8ae832518aaecbd78
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-02-12 23:37:53 +0600

    generics fk

--- tags/models.py
+++ tags/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Tag(models.Model):
@@ -11,5 +13,11 @@ class TaggedItem(models.Model):
     # Making generics to make it independent from product class. SO first content type is being used
     content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
     # id of the model
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+class LikeItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
