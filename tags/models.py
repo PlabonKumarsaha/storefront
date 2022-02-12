acb85fed55fd09ba99d09c5e8699369b47d2b0d9
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-02-12 23:11:18 +0600

    generic fk2

--- tags/models.py
+++ tags/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 # Create your models here.
 class Tag(models.Model):
     label = models.CharField(max_length=255)
@@ -10,3 +12,4 @@ class TaggedItem(models.Model):
     content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
     # id of the model
     object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
