82df4560d3678ef84611054689ef93ca5a67fe90
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-02-12 23:11:02 +0600

    generic fk

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
+    # Making generics to make it independent from product class. SO first content type is being used
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    # id of the model
+    object_id = models.PositiveIntegerField()
